a7c7328fdff20db9e6e61a5aa6cbd5fe2b936b66
IRONmanAbhi <ironmanabhinav1@gmail.com>
2024-10-29 01:31:17 +0530

    added Post Model
    created post model, registered post model for admin page, connecting user with the posts with foreign key, query data stored in database

--- blog/views.py
+++ blog/views.py
@@ -1,29 +1,9 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author' : 'Ironman',
-        'title' : 'First blog',
-        'content' : 'Blog content',
-        'date_posted' : 'Aug 24'
-    },
-    {
-        'author' : 'Batman',
-        'title' : 'Second blog',
-        'content' : 'Blog 2 content',
-        'date_posted' : 'Sep 24'
-    },
-    {
-        'author' : 'spiderman',
-        'title' : 'Third blog',
-        'content' : 'Blog 3 content',
-        'date_posted' : 'Oct 24'
-    }
-]
+from .models import Post
 
 def home(request):
     context = {
-        'posts' : posts
+        'posts' : Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
